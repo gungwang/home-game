7e7be2f9917c234047402541b2d18257f71ec307
gungwang-ul <gung.wang@ul.com>
2026-03-30 15:17:31 -0400

    AB#main: main: add readme into public.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        readme: resolve(__dirname, 'README.html'),
+      },
+    },
+  },
 })
